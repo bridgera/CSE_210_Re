9623aa062e401fa018458111821394d2af9e7da5
Bridger Allen <all22006@byui.edu>
2023-09-16 23:08:15 -0600

    Week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your Last Name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
+
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade = "";
+
+        Console.Write("What is your grade percentage? ");
+        string strPercent = Console.ReadLine();
+        float percent = int.Parse(strPercent);
+        float lastDigit = percent % 10;
+        string plusMinus = "";
+
+        // determine plusMinus
+        if (lastDigit >= 7)
+        {
+            plusMinus = "+";
+        }
+
+        else if (lastDigit < 3)
+        {
+            plusMinus = "-";
+        }
+
+        // determine letter grade
+        if (percent >= 97)
+        {
+            letterGrade = "A";
+            plusMinus = "";
+        }
+
+        else if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else if (percent < 60)
+        {
+            letterGrade = "F";
+            plusMinus = "";
+        }
+
+        Console.WriteLine($"You recieved a(n) {letterGrade}{plusMinus}");
+
+        if (percent < 70)
+        {
+            Console.WriteLine("You didn't pass! Better luck next semester ;)");
+        }
+
+        else
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
     }
 }
