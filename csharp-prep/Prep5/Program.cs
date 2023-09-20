2312bef0768a2fb71bc55cbea8f9772b80582bff
Bridger Allen <all22006@byui.edu>
2023-09-20 02:28:27 -0600

    final prep

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome(string displayMessage)
+    {
+        Console.WriteLine(displayMessage);
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static float PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string favNumStr = Console.ReadLine();
+        float favNum = float.Parse(favNumStr);
+        return favNum;
+    }
+
+    static float SquareNumber(float userNumber)
+    {
+        float squaredNumber = userNumber * userNumber;
+        return squaredNumber;
+    }
+
+    static void DisplayResult(float sqNum, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sqNum}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome("Welcome to the program!");
+        string userName = PromptUserName();
+        float userNumber = PromptUserNumber();
+        DisplayResult(SquareNumber(userNumber), userName);
+
     }
 }
