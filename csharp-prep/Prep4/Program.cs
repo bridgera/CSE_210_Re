c515f6347119831e7bebeedf19818cd8daa71d7b
Bridger Allen <all22006@byui.edu>
2023-09-20 00:32:59 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
-
+using System.Collections.Generic;
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numberList = new List<float>();
+        float number = -1;
+        float total = 0;
+        float largestNum = 0;
+        float smallestNum = 999999;
+        Console.WriteLine("Enter a list of numbers, positive and negative, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string numberStr = Console.ReadLine();
+            number = float.Parse(numberStr);
+
+            total += number;
+            numberList.Add(number);
+
+            if (number > largestNum)
+            {
+                largestNum = number;
+            }
+
+            if (number < smallestNum && number > 0)
+            {
+                smallestNum = number;
+            }
+        }
+
+        numberList.Sort();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {total / numberList.Count}");
+        Console.WriteLine($"The largest number is {largestNum}");
+        Console.WriteLine($"The smallest positive number is {smallestNum}");
+        Console.WriteLine("Here is the list in order");
+
+        foreach (float num in numberList)
+        {
+            Console.WriteLine(num);
+        }
+
+
+
+        
+       
+
     }
 }
