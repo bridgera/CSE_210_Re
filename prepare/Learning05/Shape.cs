0bec7c0cfd204f430c6d01850806ecd8429b1463
bridgera <113536070+bridgera@users.noreply.github.com>
2023-11-09 23:24:21 -0700

    prep

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,26 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public abstract double GetArea();
+    
+    public void DisplayShtuff()
+    {
+        Console.WriteLine($"The {GetColor()} shape has an area of {GetArea()}.");
+    }
+
+}
